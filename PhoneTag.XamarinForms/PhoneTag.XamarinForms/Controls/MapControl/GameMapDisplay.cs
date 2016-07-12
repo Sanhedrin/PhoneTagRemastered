53fb1651a8c8f5f7f258eb9a7de646f217e39a95
Paximillian <morzil@walla.com>
2016-07-12 14:35:14 +0300

    Added missing files.

---
+++ PhoneTag.XamarinForms/PhoneTag.XamarinForms/Controls/MapControl/GameMapDisplay.cs
@@ -0,0 +1,34 @@
+﻿using Plugin.XamJam.Screen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace PhoneTag.XamarinForms.Controls.MapControl
+{
+    /// <summary>
+    /// A specialized game map that only allows to view the play area and doesn't actually interact
+    /// with the player.
+    /// </summary>
+    public class GameMapDisplay : Map
+    {
+        public double GameRadius { get; private set; }
+        public Position StartLocation { get; private set; }
+
+        public GameMapDisplay(Position i_GameLocation, double i_GameRadius, double i_ZoomRadius) : 
+            base(MapSpan.FromCenterAndRadius(i_GameLocation, Distance.FromKilometers(i_ZoomRadius)).WithZoom(2))
+        {
+            IsShowingUser = true;
+            HeightRequest = CrossScreen.Current.Size.Height * 2 / 5;
+            WidthRequest = CrossScreen.Current.Size.Width;
+            HorizontalOptions = LayoutOptions.Fill;
+            VerticalOptions = LayoutOptions.Fill;
+
+            StartLocation = i_GameLocation;
+            GameRadius = i_GameRadius;
+        }
+    }
+}
---
+++ PhoneTag.XamarinForms/PhoneTag.XamarinForms/Pages/GameAreaDisplayPage.cs
@@ -0,0 +1,41 @@
+﻿using PhoneTag.SharedCodebase.Utils;
+using PhoneTag.XamarinForms.Controls.MapControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace PhoneTag.XamarinForms.Pages
+{
+    /// <summary>
+    /// Displays the map that was chosen for the current game without enabling any game features on it
+    /// </summary>
+    public partial class GameAreaDisplayPage : ContentPage
+    {        
+        private const double k_DefaultZoomRatio = 2;
+
+        private GameMapDisplay m_GameMap;
+
+        public GameAreaDisplayPage(Position i_StartLocation, double i_Radius)
+        {
+            setupChooserMap(i_StartLocation, i_Radius);
+
+            initializeComponent();
+        }
+
+        //Initializes the map to the last chosen location or to your current location.
+        private void setupChooserMap(Position i_StartLocation, double i_Radius)
+        {
+            m_GameMap = new GameMapDisplay(i_StartLocation, i_Radius, i_Radius * k_DefaultZoomRatio);
+        }
+
+        //When the area is chosen return to the last page.
+        private async Task DoneButton_Clicked()
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
